452eaa695293aca750c87d23739d451aebb5c678
EricPrieto <ep1043@hotmail.com>
2021-11-04 17:28:06 -0500

    Created Super Hero Table shell

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ import TitleBar from './TitleBar/TitleBar';
 import DisplayName from './DisplayName/DisplayName';
 import NameList from './NameList/NameList';
 import AlertUser from './AlertUser/AlertUser';
+import SuperHeroTable from './SuperHeroTable/SuperHeroTable';
 
 
 class App extends Component {
@@ -13,7 +14,28 @@ class App extends Component {
             firstName:"Reggie", 
             lastName: "White",
             names: ['Mike', 'Nevin', 'Aaron', 'Tory', 'Kelly'],
-            popup: alert("devCodeCamp")
+            popup: alert("devCodeCamp"),
+
+            superheroes: [
+                {
+                    superheroId: 1,
+                    name: 'Batman',
+                    primaryAbility: 'Wealthy',
+                    secondarAbility: 'Rich'
+                },
+                {
+                    superheroId: 2,
+                    name: 'Superman',
+                    primaryAbility: 'Super strength',
+                    secondarAbility: 'Fly'
+                },
+                {
+                    superheroId: 3,
+                    name: 'Spiderman',
+                    primaryAbility: 'Spider senses',
+                    secondarAbility: 'Shoots web'
+                }
+            ]
         };
     }
 
@@ -26,6 +48,7 @@ render() {
             <DisplayName name={this.state} />        
             <NameList arrayOfNames={this.state.names} />   
             <AlertUser message={this.popup} />   
+            <SuperHeroTable />
         </div>
         )
     }
---
+++ src/components/SuperHeroTable/SuperHeroTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SuperHeroTable = (props) => {
+    return(
+        <h1>Super Hero Table</h1>
+    );
+}
+
+export default SuperHeroTable;
